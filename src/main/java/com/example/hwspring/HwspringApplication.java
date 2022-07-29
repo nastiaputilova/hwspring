55f11722fe9f41f639f8f307288f4f4463c16363
anastasiaputilova <anastasiaputilova@gmail.com>
2022-07-29 03:04:20 +0100

    Initial commit

---
+++ src/main/java/com/example/hwspring/HwspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwspring/HwspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
